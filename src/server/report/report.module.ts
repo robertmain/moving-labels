a929abd59da60fd48852c9ff0ea9a97be0346b74
Robert Main <lenswipe@gmail.com>
2021-06-28 18:33:12 -0400

    Create item inventory

--- src/server/report/report.module.ts
+++ src/server/report/report.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Box } from 'server/box/box.entity';
+import { Item } from 'server/box/item.entity';
 import { ManifestController } from './manifest.controller';
+import { InventoryController } from './inventory.controller';
 import { BoxModule } from '../box/box.module';
 
 @Module({
   imports: [
     BoxModule,
+    TypeOrmModule.forFeature([Box, Item]),
   ],
   controllers: [
     ManifestController,
+    InventoryController,
   ],
   providers: [
   ],
